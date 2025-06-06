aaa5ca690834c6d65075e39ca5d463e9c5866ecd
Najma-1437 <najmaw7341@gmail.com>
2025-06-06 17:25:53 +0300

    Initial commit

---
+++ drinksystem/src/main/java/com/drinkstore/drinksystem/DrinksystemApplication.java
@@ -0,0 +1,13 @@
+package com.drinkstore.drinksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksystemApplication.class, args);
+	}
+
+}
---
+++ drinksystem/src/test/java/com/drinkstore/drinksystem/DrinksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkstore.drinksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
